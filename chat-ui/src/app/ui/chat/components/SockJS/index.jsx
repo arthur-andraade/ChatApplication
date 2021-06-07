802a2d023b7061d5c857a96ca045ef6b180e5860
Arthur Andrade <arthurandradenf@gmail.com>
2021-06-07 16:19:57 -0300

    Refactoring the conect with server

--- chat-ui/src/app/ui/chat/components/SockJS/index.jsx
+++ chat-ui/src/app/ui/chat/components/SockJS/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
+import { useUser } from "../../../../context";
+import ChatServer from "../../../../services/chat-server";
 
 const SockJSComponent = ({ url, onMessage, onUser }) => {
+    const { user } = useUser()
     const [connected, setConnected] = useState(false);
     const clientSockStompJS = useRef(null);
 
@@ -10,19 +13,32 @@ const SockJSComponent = ({ url, onMessage, onUser }) => {
         clientSockStompJS.current = Stomp.over(new SockJS(url));
         clientSockStompJS.current.connect({}, (frame) => {
             setConnected(true);
-            clientSockStompJS.current.subscribe('/topic/public', (messageFromServer) => {
-                onMessage(JSON.parse(messageFromServer.body));
-            })
-            clientSockStompJS.current.subscribe('/topic/contacts', (user) => {
-                onUser(JSON.parse(user.body));
-            })
         });
     }, []);
 
     useEffect(() => {
+        if (connected) {
+            ChatServer.post("/connect", {
+                name: user,
+                status: "on"
+            }).then(() => {
+                clientSockStompJS.current.subscribe('/topic/public', (messageFromServer) => {
+                    onMessage(JSON.parse(messageFromServer.body));
+                });
+                clientSockStompJS.current.subscribe('/topic/contacts', (user) => {
+                    onUser(JSON.parse(user.body));
+                });
+            })
+        }
+
         return () => {
             if (connected) {
-                clientSockStompJS.current.disconnect()
+                ChatServer.post("/disconnect", {
+                    user: "Exemplo",
+                    status: "off"
+                }).then(() => {
+                    clientSockStompJS.current.disconnect()
+                })
             }
         }
     }, [connected]);
